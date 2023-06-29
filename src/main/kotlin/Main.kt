f3c382605e4fbd8a962879dd51a81a36872983a6
Ana Caroline <124089800+Ana-dev19@users.noreply.github.com>
2023-06-29 15:52:07 -0300

    Delete Main.kt

--- src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    print("Digite um número inteiro: ")
-    var number = readln().toInt()
-    var antecessor = number - 1
-    println("O antecessor de $number é $antecessor")
-
-
-}
